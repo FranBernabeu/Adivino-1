28943f2e288df7cf72a3e86a1883aa62f13acb83
azord <azord@DESKTOP-MD9K6U5>
2018-10-17 16:16:10 +0200

    segunda versión

--- src/adivino/Adivino.java
+++ src/adivino/Adivino.java
@@ -1,12 +1,13 @@
 package adivino;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Adivino {
 
     public static void main(String[] args) {
         int n = 0, cont = 0;
-        int x = (int) (500 * Math.random());
+        int x =ThreadLocalRandom.current().nextInt(10, 50);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
